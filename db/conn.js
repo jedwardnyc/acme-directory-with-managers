f348ec1ae16897f8b5bda7e22666b088336b4c55
Jedwardnyc <jacobedwardrico@gmail.com>
2018-02-18 18:04:28 -0500

    fixed some bugs

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize')
-const conn = new Sequelize(process.env.DATABASE_URL)
+const conn = new Sequelize(process.env.DATABASE_URL||'postgres://localhost/dir_with_managers_db')
 
 module.exports = conn;
